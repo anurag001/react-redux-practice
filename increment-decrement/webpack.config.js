57a10988c988b90c5ee124dd563d6bd4b864c184
Anurag Kumar <anurag@internshala.com>
2020-10-04 20:31:42 +0530

    Increment Decrement tool

---
+++ increment-decrement/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    },
+    mode: 'development',
+	devServer: {
+	 contentBase: path.join(__dirname, 'public')
+	}
+};
+
